eb217fca1b43120e548751dd2f955eaf809721df
Owusu <adjeibohyen@hotmail.co.uk>
2023-02-21 09:35:55 +0000

    wip: partial implementation of about app

--- src/about/models.py
+++ src/about/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    image_base64_title = models.CharField(max_length=25)
+    image_base64_alt = models.CharField(max_length=50)
+    image_base64_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Link(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    link_icon_title = models.CharField(max_length=25)
+    link_icon_alt = models.CharField(max_length=50)
+    link_icon_href = models.CharField(max_length=250)
+    link_icon_base64_url = models.TextField()
+
+    def __str__(self):
+        return self.link_icon_title
+
+
+class Timeline(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    modified_on = models.DateTimeField(auto_now_add=True)
+    header = models.CharField(max_length=120, unique=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.header

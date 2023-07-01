a55cc0e7a2ce28d520e0071f19d6b9fa83bf5cf0
Owusu <adjeibohyen@hotmail.co.uk>
2023-07-01 01:01:26 +0100

    fix: rm redundant field

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Author, Post, Tag
+from blog.models import Post, Tag
 
-admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(Post)

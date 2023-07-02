60659b64f869f916062f15f4adb11403c9360dfa
Owusu <adjeibohyen@hotmail.co.uk>
2023-07-02 21:29:12 +0100

    chore: integrate django debug toolbar

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path, reverse
@@ -9,3 +11,9 @@ urlpatterns = [
     path("exclusive/", include(("exclusive.urls", "exclusive"), namespace="exclusive")),
     path("", lambda _: redirect(reverse("blog:index"))),
 ]
+
+if os.environ.get("APP_ENV") == "development":
+    dev_urlpatterns = [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+    urlpatterns = urlpatterns + dev_urlpatterns

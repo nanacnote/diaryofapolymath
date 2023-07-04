c18156146c81ad0ee14660046013cda12dfb17c5
Owusu <adjeibohyen@hotmail.co.uk>
2023-07-04 20:51:20 +0100

    chore: add tests for exclusive app

--- src/exclusive/tests.py
+++ src/exclusive/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+class TestExclusiveModels:
+    def test_XXX_model_has_expected_fields_and_constraints(self):
+        pass
+
+
+@pytest.mark.django_db
+class TestExclusiveViews:
+    def test_index_page_renders_correctly(self, client):
+        response = client.get(reverse("exclusive:index"))
+
+        assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+class TestExclusiveUtils:
+    def test_XXX_utility_as_a_unit(self):
+        pass

ebaa77c270db919768d5c3fa1ee789461a6154e1
Owusu <adjeibohyen@hotmail.co.uk>
2023-07-04 20:49:39 +0100

    feat: get view context from locals

--- src/about/views.py
+++ src/about/views.py
@@ -4,10 +4,9 @@ from .models import Profile
 
 
 def index(request):
-    result = Profile.objects.get_profile_links_timelines_for_superuser()
-    context = dict(
-        profile=result[0],
-        links=result[1],
-        timelines=result[2],
-    )
-    return render(request, "about/index.html", context)
+    (
+        profile,
+        links,
+        timelines,
+    ) = Profile.objects.get_profile_links_timelines_for_superuser()
+    return render(request, "about/index.html", locals())

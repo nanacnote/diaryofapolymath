8ef347640c06f317a0e1cdf6091303954d8c9439
Owusu <adjeibohyen@hotmail.co.uk>
2023-11-25 18:21:05 +0000

    chore: rename env variable

--- src/base/settings/production.py
+++ src/base/settings/production.py
@@ -5,7 +5,7 @@ from base.settings.base import *
 DEBUG = False
 STATIC_ROOT = "/var/www/"
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-ALLOWED_HOSTS = [os.environ.get("NGINX_SERVER_NAME")]
+ALLOWED_HOSTS = [os.environ.get("DJANGO_DOMAIN_NAME")]
 
 DATABASES = {
     "default": {

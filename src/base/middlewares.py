6b8eb9aae11339f62ff338cca3b4287826faab48
nanacnote <adjeibohyen@hotmail.co.uk>
2024-06-03 22:16:45 +0100

    feat: add goatcounter html script tag

---
+++ src/base/middlewares.py
@@ -0,0 +1,22 @@
+import os
+
+
+class AddGoatcounterScript:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if "text/html" in response.get("Content-Type", ""):
+            goatcounter_url = os.environ.get("GOATCOUNTER_DOMAIN_NAME")
+            script = "<script"
+            f'data-goatcounter="http://{goatcounter_url}/count" '
+            f'async src="//{goatcounter_url}/count.js"></script>'
+            closing_body_index = response.content.find(b"</body>")
+            if closing_body_index != -1:
+                response.content = (
+                    response.content[:closing_body_index]
+                    + script.encode()  # noqa: W503
+                    + response.content[closing_body_index:]  # noqa: W503
+                )
+        return response

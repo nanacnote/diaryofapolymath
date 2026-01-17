b2a4dce1158128038ccf15ae794d2a9b1878da59
nanacnote <adjeibohyen@hotmail.co.uk>
2026-01-17 13:13:18 +0000

    chore: add session cookie engine

--- src/base/settings/base.py
+++ src/base/settings/base.py
@@ -76,3 +76,5 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "base" / "static"]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "about.Profile"
+
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"

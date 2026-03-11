c21623d5c9af6bda9217becc31dd0f438f3172c7
nanacnote <adjeibohyen@hotmail.co.uk>
2026-03-11 23:17:02 +0000

    feat: impl rss and atom feeds

--- src/base/settings/base.py
+++ src/base/settings/base.py
@@ -10,6 +10,7 @@ DJANGO_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.syndication",
 ]
 
 EXTERNAL_APPS = []
---
+++ src/blog/feeds.py
@@ -0,0 +1,42 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
+from django.utils.feedgenerator import Atom1Feed as AtomFeed
+from django.utils.feedgenerator import Rss201rev2Feed as RssFeed
+
+from .models import Post
+
+
+class LatestEntriesFeed(Feed):
+    # TODO: find a way to get this from the context processor instead of hardcoding it here
+    title = "Diary of a Polymath"
+    description = ""
+
+    @property
+    def link(self):
+        return reverse("blog:index")
+
+    def items(self):
+        return Post.objects.get_published_posts()
+
+    def item_title(self, item):
+        return f"{item.title} | {item.subtitle}"
+
+    def item_description(self, item):
+        return item.abstract
+
+    def item_pubdate(self, item):
+        return item.published_on
+
+    def item_categories(self, item):
+        return [tag.name for tag in item.tags.all()]
+
+    def item_link(self, item):
+        return reverse("blog:post", args=[item.slug])
+
+
+class RssLatestPostsFeed(LatestEntriesFeed):
+    feed_type = RssFeed
+
+
+class AtomLatestPostsFeed(LatestEntriesFeed):
+    feed_type = AtomFeed

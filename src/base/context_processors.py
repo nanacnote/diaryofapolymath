5d2d7baafb3441983b6e4bdfe338a16eaf83b4fc
nanacnote <adjeibohyen@hotmail.co.uk>
2026-03-22 00:38:55 +0000

    feat: update copy and headers

--- src/base/context_processors.py
+++ src/base/context_processors.py
@@ -1,5 +1,6 @@
 def session_theme_processor(request):
-    session_theme = request.session.get("theme", "light")
+    color_scheme = getattr(request, "color_scheme", "light")
+    session_theme = request.session.get("theme", color_scheme)
     return {"session_theme": session_theme}
 
 
@@ -7,10 +8,16 @@ def copy_processor(request):
     return {
         "copy": {
             "meta_site_name": "Diary of a Polymath",
-            "meta_site_description": "",
+            "meta_site_description": "A personal knowledge journal and publishing space.",
             "nav_about": "About",
             "nav_blog": "Blog",
             "nav_exclusive": "Exclusive",
-            "footer_powered_by": "Powered by",
+            "common_powered_by": "Powered by",
+            "common_tagged": "Tagged",
+            "common_archived": "Archived",
+            "common_shared_by": "Shared by",
+            "common_like": "Like",
+            "common_comment": "Comment",
+            "common_copy_link": "Copy link",
         }
     }

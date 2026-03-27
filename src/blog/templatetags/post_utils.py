a91d24831e552ee5f937517f59348ddee756601d
nanacnote <adjeibohyen@hotmail.co.uk>
2026-03-27 22:03:44 +0000

    feat: add timeago and email to name parser

--- src/blog/templatetags/post_utils.py
+++ src/blog/templatetags/post_utils.py
@@ -1,4 +1,6 @@
+import timeago
 from django import template
+from django.utils import timezone
 from readtime import of_text
 
 register = template.Library()
@@ -7,3 +9,13 @@ register = template.Library()
 @register.filter
 def read_time(content):
     return of_text(content)
+
+
+@register.filter
+def time_ago(since):
+    return timeago.format(since, timezone.now())
+
+
+@register.filter
+def email_to_name(value):
+    return str(value).split("@", 1)[0].replace(".", " ").replace("_", " ").title()

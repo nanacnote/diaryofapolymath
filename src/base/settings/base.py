c0369b713144e5afc57709fcf964cbb9a65a3b19
nanacnote <adjeibohyen@hotmail.co.uk>
2026-04-03 12:32:23 +0100

    chore: add thread id to logger

--- src/base/settings/base.py
+++ src/base/settings/base.py
@@ -88,7 +88,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "standard": {
-            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            "format": "%(asctime)s [%(levelname)s] [%(threadName)s] %(name)s: %(message)s",
         },
     },
     "handlers": {
